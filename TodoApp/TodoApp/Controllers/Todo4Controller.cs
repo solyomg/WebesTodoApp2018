daeb2588c563eede7b30fdb686a4d20e9f00e3e4
Gábor Sólyom <solyomg@freemail.hu>
2018-11-02 17:18:06 +0100

    #3: varázsló használata

---
+++ TodoApp/TodoApp/Controllers/Todo4Controller.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    public class Todo4Controller : Controller
+    {
+        private Db db = new Db();
+
+        // GET: Todo4
+        public ActionResult Index()
+        {
+            return View(db.TodoItems.ToList());
+        }
+
+        // GET: Todo4/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TodoItem todoItem = db.TodoItems.Find(id);
+            if (todoItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todoItem);
+        }
+
+        // GET: Todo4/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Todo4/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Done")] TodoItem todoItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TodoItems.Add(todoItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(todoItem);
+        }
+
+        // GET: Todo4/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TodoItem todoItem = db.TodoItems.Find(id);
+            if (todoItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todoItem);
+        }
+
+        // POST: Todo4/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Done")] TodoItem todoItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(todoItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(todoItem);
+        }
+
+        // GET: Todo4/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TodoItem todoItem = db.TodoItems.Find(id);
+            if (todoItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todoItem);
+        }
+
+        // POST: Todo4/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TodoItem todoItem = db.TodoItems.Find(id);
+            db.TodoItems.Remove(todoItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
